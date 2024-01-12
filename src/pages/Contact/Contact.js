4c50906274fd33e0e96a56a4f6268432ebd58de2
Hautrung1608 <hautrung16082003@gmail.com>
2024-01-12 09:55:58 +0700

    Update Contact.js

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 
 
 const Contact = () => {
-  // ========== Error Messages Start here ============
+  
   return (
     <div className="max-w-container mx-auto px-4">
       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14894.506820152996!2d105.79335975!3d21.0476174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1705027956361!5m2!1svi!2s" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
